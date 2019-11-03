37bf35919acf41061f76e635cbf2fbbc12001bae
Ojasvi Shaklya <34879647+ojasvishaklya@users.noreply.github.com>
2019-11-03 16:46:21 +0530

    Add files via upload

---
+++ Ziegler Nicol.m
@@ -0,0 +1,38 @@
+close all;
+clear all;
+num=[1];
+den=[5 15.5 11.5 1];
+sys=tf(num,den);
+[y,t] =step(sys);
+plot(t,y,'LineWidth',2); grid on; xlabel('Time(s)'); ylabel('Amplitude');
+
+hold on;
+K=34.65;
+T=4.143;
+
+%P-Controller
+Kp=0.5*K;
+Ki=0;
+Kd=0;
+cont=pid(Kp,Ki,Kd);
+cl_sys=feedback(sys*cont,1);
+[y,t] =step(cl_sys);
+plot(t,y,'LineWidth',2); grid on; xlabel('Time(s)'); ylabel('Amplitude');
+
+%PI-Controller
+Kp=0.45*K;
+Ki=T/1.2;
+Kd=0;
+cont=pid(Kp,1/Ki,Kd);
+cl_sys=feedback(sys*cont,1);
+[y,t] =step(cl_sys);
+plot(t,y,'LineWidth',2); grid on; xlabel('Time(s)'); ylabel('Amplitude');
+
+%PID-Controller
+Kp=0.6*K;
+Ki=T/2;
+Kd=T/8;
+cont=pid(Kp,1/Ki,Kd);
+cl_sys=feedback(sys*cont,1);
+[y,t] =step(cl_sys);
+plot(t,y,'LineWidth',2); grid on; xlabel('Time(s)'); ylabel('Amplitude');
