f5ee6e5ff386b26b0d30ff1eea3909c48ede3862
Rishu Anand <riuandg5@gmail.com>
2019-11-09 06:28:55 +0530

    correct Ki and Kd

--- Ziegler Nicol.m
+++ Ziegler Nicol.m
@@ -13,27 +13,24 @@ T = 2*pi/Pcf; % time period of sustained oscillations
 
 %P-Controller
 Kp=0.5*K;
-Ki=0;
-Kd=0;
-cont=pid(Kp,Ki,Kd);
+cont=pid(Kp);
 cl_sys=feedback(sys*cont,1);
 [y,t] =step(cl_sys);
 plot(t,y,'LineWidth',2); grid on; xlabel('Time(s)'); ylabel('Amplitude');
 
 %PI-Controller
-Kp=0.45*K;
-Ki=T/1.2;
-Kd=0;
-cont=pid(Kp,1/Ki,Kd);
+Kp=0.4*K;
+Ki=Kp/(0.8*T);
+cont=pid(Kp,Ki);
 cl_sys=feedback(sys*cont,1);
 [y,t] =step(cl_sys);
 plot(t,y,'LineWidth',2); grid on; xlabel('Time(s)'); ylabel('Amplitude');
 
 %PID-Controller
 Kp=0.6*K;
-Ki=T/2;
-Kd=T/8;
-cont=pid(Kp,1/Ki,Kd);
+Ki=Kp/(0.5*T);
+Kd=Kp*0.12*T;
+cont=pid(Kp,Ki,Kd);
 cl_sys=feedback(sys*cont,1);
 [y,t] =step(cl_sys);
 plot(t,y,'LineWidth',2); grid on; xlabel('Time(s)'); ylabel('Amplitude');
