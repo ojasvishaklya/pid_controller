733aa86b9dc91ee4540f8ea6852d6683d5e2de96
Rishu Anand <riuandg5@gmail.com>
2019-11-09 06:33:40 +0530

    format code and add comments

--- Ziegler Nicol.m
+++ Ziegler Nicol.m
@@ -1,36 +1,53 @@
 close all;
-clear all;
-num=[1];
-den=[5 15.5 11.5 1];
-sys=tf(num,den);
-[y,t] =step(sys);
-plot(t,y,'LineWidth',2); grid on; xlabel('Time(s)'); ylabel('Amplitude');
+clear;
 
-hold on;
+% open loop plant
+Gp = tf(1, [5 15.5 11.5 1]);
+
+% maximize figure window to use subplots
+figure('units', 'normalized', 'outerposition', [0 0 1 1]);
+
+% plant vs sys
+subplot(2, 2, 1);
+Gc = 1;
+plant_vs_sys(Gp, Gc, 1);
+title('Plant Vs System');
+
+% stability margins of plant
 [GM, PM, Pcf, Gcf] = margin(Gp);
-K = GM; % gain at which sys is marginally stable
-T = 2*pi/Pcf; % time period of sustained oscillations
+Ku = GM; % gain at which sys is marginally stable
+Tu = 2*pi/Pcf; % time period of sustained oscillations
 
-%P-Controller
-Kp=0.5*K;
-cont=pid(Kp);
-cl_sys=feedback(sys*cont,1);
-[y,t] =step(cl_sys);
-plot(t,y,'LineWidth',2); grid on; xlabel('Time(s)'); ylabel('Amplitude');
+% plant vs sys with P controller
+subplot(2, 2, 2);
+Kp = 0.5*Ku;
+Gc = pid(Kp);
+plant_vs_sys(Gp, Gc, 1);
+title('Plant Vs System with P');
 
-%PI-Controller
-Kp=0.4*K;
-Ki=Kp/(0.8*T);
-cont=pid(Kp,Ki);
-cl_sys=feedback(sys*cont,1);
-[y,t] =step(cl_sys);
-plot(t,y,'LineWidth',2); grid on; xlabel('Time(s)'); ylabel('Amplitude');
+% plant vs sys with PI controller
+subplot(2, 2, 3);
+Kp = 0.4*Ku;
+Ki = Kp/(0.8*Tu);
+Gc = pid(Kp, Ki);
+plant_vs_sys(Gp, Gc, 1);
+title('Plant Vs System with PI');
 
-%PID-Controller
-Kp=0.6*K;
-Ki=Kp/(0.5*T);
-Kd=Kp*0.12*T;
-cont=pid(Kp,Ki,Kd);
-cl_sys=feedback(sys*cont,1);
-[y,t] =step(cl_sys);
-plot(t,y,'LineWidth',2); grid on; xlabel('Time(s)'); ylabel('Amplitude');
+% plant vs sys with PID controller
+subplot(2, 2, 4);
+Kp = 0.6*Ku;
+Ki = Kp/(0.5*Tu);
+Kd = Kp*0.12*Tu;
+Gc = pid(Kp, Ki, Kd);
+plant_vs_sys(Gp, Gc, 1);
+title('Plant Vs System with PID');
+
+% function to plot step response of plant and closed loop system
+function [] = plant_vs_sys(gp, gc, f)
+    sys = feedback(gc*gp, f); % closed loop system
+    hold on;
+    step(gp);
+    step(sys);
+    legend;
+    hold off;
+end
